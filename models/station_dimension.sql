4c1f4ee88eb129b3602ed539cf6405680728cee5
Hoang Pham <hpham27th3@gmail.com>
2025-12-31 10:02:57 +0000

    new change with station_dimension model

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -6,7 +6,7 @@ WITH BIKE AS (
     start_station_name AS station_name,
     start_lat AS station_lat,
     start_lng AS start_station_lng
-FROM {{source('demo','bike')}}
+FROM {{ref('stg_bike')}}
 
 WHERE RIDE_ID != 'ride_id'
 
